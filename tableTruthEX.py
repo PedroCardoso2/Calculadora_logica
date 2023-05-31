dad4d2ed27b2cd1d5aa059e492cba87ff493b143
PedroVíctorMCB <pedrovictormcb80@gmail.com>
2023-05-31 12:41:40 -0300

    Criei um arquivo py como exemplo de tabela verdade

---
+++ tableTruthEX.py
@@ -0,0 +1,8 @@
+//Este é um exemplo, apenas um exemplo, de como imagino o programa funcionando.
+//Abstraia de certas coisas que não domino
+
+//aqui estamos fazendo uma entrada da expressão lógica. No js vocês vão usar os botões para fazer este input
+txtLogc = input('Digite uma expressão lógica: ')
+
+//estou criando uma lista para fazer o fatiamento de string, pois não sei fazer botões e associá-los a valores
+//você farão isso com os botões
